38069e95c3ea2916625a7b9dc14a072185a529e8
specialpics8-mac <specialpics8@gmail.com>
2026-06-28 20:37:00 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'paisa-v2';
+const ASSETS = ['/index.html', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => Promise.allSettled(ASSETS.map(u => c.add(u).catch(()=>{})))));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.url.includes('script.google.com') || e.request.url.includes('fonts.g')) {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+    return;
+  }
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+    caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+    return res;
+  })));
+});
